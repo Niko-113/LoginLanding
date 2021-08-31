a3f0b23ecd642d987ee0dfc2c42460c9a04e2d2b
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-08-31 09:59:52 -0700

    Landing Placeholders

--- app/src/main/java/com/example/loginlanding/MainActivity.java
+++ app/src/main/java/com/example/loginlanding/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.loginlanding;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_landing);
+
+
     }
 }
