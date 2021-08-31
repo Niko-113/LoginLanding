2b8d400a02624def052289a080fc11c0bbbb5ad5
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-08-31 10:25:45 -0700

    Factory Pattern

--- app/src/main/java/com/example/loginlanding/MainActivity.java
+++ app/src/main/java/com/example/loginlanding/MainActivity.java
@@ -2,17 +2,32 @@ package com.example.loginlanding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LandingActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
